f57ec35abc36a368f99d09ee3f46d8e209afaf51
Akhil <55911943+nvakhilnair@users.noreply.github.com>
2020-06-23 18:34:51 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+
+GUI application is used get price alert for amazon and flipkart through email-id
+
+Requirement: PyQt4==4.11.4
+
+*You can also manually install these libraries using <pip install PyQt4>
+or use Requirements.txt file using <pip install -r Requirements.txt>
+
+Feedbacks and suggestions for improvement are always welcomed
+email id: MadeWithPY009@gmail.com 
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="covid123",
+    version="1.0.0",
+    description="GUI application is used get price alert for amazon and flipkart through email-id",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nvakhilnair/",
+    author="Akhil",
+    author_email="nvakhilnair@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=['pythonfiles'],
+    include_package_data=True,
+    install_requires=["PyQt4>=4.11.4"],
+    entry_points={},
+)
